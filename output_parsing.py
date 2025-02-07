73df064b316e76c0bb362e0ca02e876f17568185
Ann M Hagan <39930638+anniepuy@users.noreply.github.com>
2025-02-07 12:14:52 -0600

    Created a pyndantic output parser to format LLM output

---
+++ output_parsing.py
@@ -0,0 +1,76 @@
+"""
+Title: LangChain Output Parsing
+Purpose: Learn how to structure the LLM output through using the output parsers.Pydantic is used to validate the data.
+Author: Ann Hagan - via learning through Laxmi Kant on Udemy
+"""
+
+from dotenv import load_dotenv
+from langchain_ollama import ChatOllama
+from langchain_core.prompts import (
+    PromptTemplate
+)
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+#base set up
+base_url = "http://localhost:11434"
+
+model = 'llama3.2'
+
+llm = ChatOllama(
+    base_url=base_url,
+    model = model,
+    temperature = 0.8,
+    num_predict = 256,
+)
+#1. How to create a Pydantic Output Parser to give LLM instructions on how to output the text
+#import all the necessary pydantic classes - BaseModel is always used
+from typing import Optional
+from pydantic import BaseModel, Field
+from langchain_core.output_parsers import PydanticOutputParser
+
+#create pydantic class - the LLM sends AI messages and then to Pydantic. Extend the BaseModel with custom method.
+#in this example, the pydantic class will be a method that validates a Joke.
+class Joke(BaseModel):
+        """
+        Joke to tell user
+        """
+        #Description must be self explanatory for LLM
+        #must pass each three of these b/c they become parameters
+        setup: str = Field(description="The set up of the joke.")
+        punchline: str = Field(description="The punchline of the joke.")
+        rating: Optional[int] = Field(description="The rating of the joke is from 1 to 10.")
+
+parser = PydanticOutputParser(pydantic_object=Joke)
+
+#this becomes the instructions to pass to the LLM for output schema
+instruction = parser.get_format_instructions()
+
+#Verify
+print(instruction)
+
+#2. Use the Pydantic Parser with Prompt Template
+#Paritial variables are the instructions for llm
+#query comes from the chain
+prompt = PromptTemplate(
+        template = '''
+        Answer the user query with a joke. Here is your formatting instruction.
+        {format_instruction}
+
+        Query: {query}
+        Answer: 
+        ''',
+        input_variables = ['query'],
+        partial_variables = {'format_instruction': parser.get_format_instructions()}
+)
+
+#View the prompt
+#print(prompt)
+
+#This prompt will be used to prepare the chain
+
+chain = prompt | llm 
+
+output = chain.invoke({'query': 'Tell me a joke about penquins.'})
+print(output.content)

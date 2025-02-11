37dd92c9365877f21f0c1d9f73c877f2a2adc614
Ann M Hagan <39930638+anniepuy@users.noreply.github.com>
2025-02-11 10:54:09 -0600

    PPT extract with prompting to write a script from the PPT

--- PPTX_data_loader.py
+++ PPTX_data_loader.py
@@ -10,8 +10,8 @@ from langchain_community.document_loaders import UnstructuredPowerPointLoader
 loader = UnstructuredPowerPointLoader("ms_data/ml_course.pptx", mode='elements')
 
 docs = loader.load()
-print(len(docs))
-print(docs[0].metadata)
+#print(len(docs))
+#print(docs[0].metadata)
 #read the documents page-wise through making a dictionary - combines all element
 ppt_data = {}
 for doc in docs:
@@ -19,11 +19,21 @@ for doc in docs:
     ##if a page is empty, it will return an empty string 
     ppt_data[page] = ppt_data.get(page, "") + "\n\n" + doc.page_content
 
-print(ppt_data)
+#print(ppt_data)
 
 #prepare content slide -wise so it is easier to read - final output. Nice!!
 context = ""
 for page, content in ppt_data.items():
     context += f"### {page}:\n\n{content.strip()}\n\n"
 
-print(context)
+#print(context)
+#
+##Write the LLM code
+from scripts import llm_qa_script
+
+##Prepare a question
+question = """
+For each PowerPoint slide provided above, write a 2-minute script from a that effectively conveys the key points. Ensure a smooth flow between slides, maintaining a clear and engaging narrative. """ 
+
+response = llm_qa_script.ask_llm(context, question)
+print(response)

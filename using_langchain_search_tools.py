1527aaeb2c63a8c90913bbd832b453b062b269e0
Ann M Hagan <39930638+anniepuy@users.noreply.github.com>
2025-02-12 14:30:14 -0600

    implemented sevarl search uses with LangChain

--- using_langchain_search_tools.py
+++ using_langchain_search_tools.py
@@ -25,6 +25,48 @@ llm = ChatOllama(
 )
 
 ## Using DuckDuckGo Search 
+from langchain_community.tools import DuckDuckGoSearchRun
 
+search = DuckDuckGoSearchRun()
 
+result = search.invoke("What is today's stock market news?")
+
+print(f"Duck duck go: {result}")
+
+##Using Taviley
+from langchain_community.tools import TavilySearchResults
+
+tavily_search = TavilySearchResults(
+    max_results =5,
+    search_depth = "advanced",
+    include_answer=True,
+    include_raw_content=True,
+)
+
+tavily_question = "What is today's stock market news?"
+
+tavily_result = tavily_search.invoke(tavily_question)
+print('\n\n')
+print(f"Tavily: {tavily_result}")
+
+### Using Wikipedia
+from langchain_community.tools import WikipediaQueryRun
+from langchain_community.utilities import WikipediaAPIWrapper
+
+wiki_search = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
+
+wiki_question = "What is today's stock market news?"
+
+wiki_result = wiki_search.invoke(wiki_question)
+print('\n\n')
+print(f"Wiki: {wiki_result}")
+
+### Using PubMed
+from langchain_community.tools import PubmedQueryRun
+pubmed_search = PubmedQueryRun()
+
+pubmed_question = "What is latest research on COVID-19?"
+
+print('\n\n')
+print(f"PubMed: {pubmed_search.invoke(pubmed_question)}")
 
